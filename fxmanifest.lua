e0066f9ae3ccb409b11ce2a3e00b91ea5c7fbe44
Jeffrey C <spaz926@me.com>
2021-05-25 03:19:53 -0500

    Update resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TerbSEC'
+description 'Sit and lay on beds and chairs'
+version '0.6'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+
+server_script 'server.lua'
